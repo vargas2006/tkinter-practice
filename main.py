71317c8bbc40ec64b762b6455c4de2b4808ef1b7
vargas2006 <johnleevargas25@gmail.com>
2026-09-04 10:55:08 +0800

    feat:place window on computer screen

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from tkinter import *
 
+# widgets = GUI elements: buttons, textboxes, labels, images
+# windows = serves as a container to hold or contain these widgets
 
 
+window = Tk() # make a window
+window.geometry("420x420") # adjust the size of the window
+window.title("tkinter practice") # change title of the window
+
+icon = PhotoImage(file='logo.png')
+window.iconphoto(True,icon) # change the icon of the window
+window.config(background="black") # change the background color of the window
+
+
+
+window.mainloop() # display window / place window on computer screen, listen for events
